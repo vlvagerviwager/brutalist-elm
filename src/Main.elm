dc02aef581070b190239f737df96f1b4fabb5278
zky829 <zky829@users.noreply.github.com>
2019-02-09 21:53:09 +0000

    Add a logo and a link.

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,33 @@
 import Browser
-import Html exposing (Html, button, div, footer, p, text)
+import Html exposing (Html, a, button, div, footer, header, img, nav, node, p, text)
+import Html.Attributes exposing (height, href, src, width)
+
+appendYas: String -> String
+appendYas string = string ++ " YASSSSS."
 
 -- VIEW
 view model =
-  div []
+  node "body" []
+    [ header []
+      [ img [ src "./src/img/logo.png", width 80, height 80 ] []
+        -- , nav []
+        -- [ text "home"
+        --   , text "about"
+        --   , text "contact"
+        -- ]
+      ]
+    , div []
     [ p [] [ text "hi." ]
-    , p [] [ text "I really like Elm and I am learning it." ]
-    , p [] [ text "We try too hard to be perfect and to make everything look good." ]
-    , p [] [ text "This is going to be a weird, ugly, brutalist site." ]
-    , button [ ] [ text "This does nothing." ]
-    , p [] []
+      , p [] [ text ( appendYas "I really like Elm and I am learning it." ) ]
+      , p [] [ text "We try too hard to be perfect and to make everything look good." ]
+      , p [] [ text "This is going to be a weird, ugly, brutalist site." ]
+      , p [] [ text "The logo was generated using "
+        , a [ href "https://hatchful.shopify.com" ] [ text "Hatchful" ]
+        , text "."
+      ]
+      , button [ ] [ text "This does nothing." ]
+      , p [] []
+    ]
     , footer [] [ text "© 2019 zky829" ]
     ]
 
