6f482fba0e9a5853c127dc4740411833dc85f7b5
zky829 <zky829@users.noreply.github.com>
2019-02-10 14:43:34 +0000

    Use Browser.sandbox and toggle some text on the page

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,39 @@
 import Browser
 import Html exposing (Html, a, button, div, footer, header, img, nav, node, p, text)
 import Html.Attributes exposing (height, href, src, width)
+import Html.Events exposing (onClick)
+
 
 appendYas: String -> String
 appendYas string = string ++ " YASSSSS."
 
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+-- MODEL
+
+type alias Model = String
+
+init : Model
+init =
+  "hi."
+
+-- UPDATE
+
+type Msg = Sunny | Rainy
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Sunny ->
+      "hi."
+    Rainy ->
+      "ugh."
+
 -- VIEW
-view =
-  node "body" []
+view : Model -> Html Msg
+view model =
+  -- Replace with Browser.Document when it does more
+  div []
     [ header []
       [ img [ src "./src/img/logo.png", width 80, height 80 ] []
         -- , nav []
@@ -17,7 +43,7 @@ view =
         -- ]
       ]
     , div []
-    [ p [] [ text "hi." ]
+    [ p [] [ text model ]
       , p [] [ text ( appendYas "I really like Elm and I am learning it." ) ]
       , p [] [ text "We try too hard to be perfect and to make everything look good." ]
       , p [] [ text "This is going to be a weird, ugly, brutalist site." ]
@@ -25,11 +51,9 @@ view =
         , a [ href "https://hatchful.shopify.com" ] [ text "Hatchful" ]
         , text "."
       ]
-      , button [ ] [ text "This does nothing." ]
+      , button [ onClick Rainy ] [ text "This does nothing." ]
       , p [] []
     ]
     , footer [] [ text "© 2019 zky829" ]
     ]
 
-main =
-    view
